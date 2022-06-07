96768bff4089a8d66ffdb87048733768c63902c9
@BrunoQuadros95 <bruno.quadros@compasso.com.br>
2022-06-06 22:47:07 -0300

    teste

---
+++ index.php
@@ -0,0 +1,347 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Grupo Partnership</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body style="overflow-x: hidden;">
+
+ 
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1><a href="index.html">Partnership</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active" href="index.html">Home</a></li>
+          <li><a href="about.html">Quem somos</a></li>
+          <li><a href="services.html">Serviços</a></li>
+          <li><a href="portfolio.html">Portfolio</a></li>
+          <li><a href="contact.html">Fale Conosco</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+        
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Bem-vindo ao <span>Grupo Partnership</span> <img src="assets/img/favicon.png"></h2>
+                <p class="animate__animated animate__fadeInUp"> Nosso propósito é <strong>agregar valores</strong> a pessoas e
+                 empresas conectando projetos 
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 2 -->
+          
+          <!--<div class="carousel-item" style="background-image: url(assets/img/slide/slide-2.jpg)">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated fanimate__adeInDown">Lorem <span>Ipsum Dolor</span></h2>
+                <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+              </div>
+            </div>
+          </div>-->
+
+          <!-- Slide 3 -->
+          <!--<div class="carousel-item" style="background-image: url(assets/img/slide/slide-3.jpg)">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Sequi ea <span>Dime Lara</span></h2>
+                <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+              </div>
+            </div>
+          </div>-->
+
+        </div>
+
+        <!--<a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+        </a>
+
+        <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+        </a>-->
+
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Featured Section ======= -->
+    <section id="featured" class="featured">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="icon-box">
+              <p class="text-center"><i class="bi bi-lightning"></i></p>
+              <h3 class="text-center"><a href="#qmSomos">Energia</a></h3>
+              <p class="text-center">
+                <a href="#qmSomos" class="btn btn-lg btn btn-outline-secondary">Saiba mais</a>
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 mt-4 mt-lg-0">
+            <div class="icon-box">
+              <p class="text-center"><i class="bi bi-pc-display"></i></p>
+              <h3 class="text-center"><a href="#tec">Tecnologia</a></h3>
+              <p class="text-center">
+                <a href="#tec" class="btn btn-lg btn btn-outline-secondary">Saiba mais</a>
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 mt-4 mt-lg-0">
+            <div class="icon-box">
+              <p class="text-center"><i class="bi bi-gear-fill"></i></p>
+              <h3 class="text-center"><a href="#servicos">Prestação de serviços</a></h3>
+              <p class="text-center">
+                <a href="#servicos" class="btn btn-lg btn btn-outline-secondary">Saiba mais</a>
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Featured Section -->
+
+    <section class="mx-8" id="qmSomos">
+      <div class="container px-5">
+          <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
+              <div class="col-sm-8 col-md-6 mt-5">
+                  <div class="px -4px-sm-0"><img class="img-fluid"
+                     src="assets/img/slide/img21.png" alt="..." /></div>
+              </div>
+
+              <div class="col-12 col-lg-5">
+                <h2 class="display-4 lh-1 mb-4">Energia</h2>
+                <p  class="lead fw-normal mb-5 mb-lg-0">Te information about your application
+                  , why it was built, the problem it solves, or anything else! There's plenty of 
+                  space for text here, so don't worry about writing too much. </p>
+                  <!--<br><a href="#" class="btn btn-lg btn-more">Saiba mais</a>-->
+             </div>
+              
+          </div>
+      </div>
+    </section>
+
+    <section class="mx-7" id="servicos">
+      <div class="container px-5">
+          <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">       
+              <div class="col-12 m-5">
+                  <h2 class="display-4 lh-1 mb-4 text-center py-5" >Prestação de serviços</h2>
+                  <p class="lead fw-normal mb-5 mb-lg-0 text-center px-5" style="color:#000" >
+                    Te information about <span style="color:#fff">your</span> 
+                    application, why it was built, the problem it solves, or anything else! 
+                    's plenty of space for text here, so don't worry about writing too much.</p>
+                    <!--<br><a href="#" class="btn btn-lg btn-more">Saiba mais</a>-->
+               </div> 
+          </div>
+      </div>
+    </section>
+
+    <section class=" mx-7" id="tec">
+      <div class="container px-5">
+          <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
+              
+            <div class="col-12 col-lg-5">
+                <h2 class="display-4 lh-1 mb-4">Tecnologia</h2>
+                <p class="lead fw-normal text-muted mb-5 mb-lg-0">Te information about your application
+                  , why it was built, the problem it solves, or anything else! There's plenty of 
+                  space for text here, so don't worry about writing too much.</p>
+                  <!--<br><a href="#" class="btn btn-lg btn-more">Saiba mais</a>-->
+              </div>
+              <div class="col-sm-8 col-md-6 mt-5">
+                <div class="px-4 px-sm-0"><img class="img-fluid" src="https://www.inovarpublico.com.br/wp-content/uploads/2017/11/laptop-png-6759-1024x614.png" alt="..." /></div>
+             </div>
+          </div>
+      </div>
+    </section>
+
+    <!-- ======= Services Section ======= -->
+    <!--<section id="services" class="services">
+      <div class="">
+
+        <div class="row" >
+          <div class="col-lg-6 col-md-6 mt-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4><a href="">Certificados</a></h4>
+              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+              <br><a href="#" class="btn btn-lg btn-saibaMais">Saiba mais</a>
+            </div>
+          </div>
+
+          <div class="col-lg-6 col-md-6 mt-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bi bi-chat-left-dots"></i></div>
+              <h4><a href="">Entre em contato</a></h4>
+              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+              <br><a href="contact.html" class="btn btn-lg btn-saibaMais">Saiba mais</a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>--><!-- End Services Section -->
+
+    
+
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients">
+      <div class="container">
+
+        <div class="text-center">
+          <h3 class="display-6 lh-1 mb-5 mt-5">Clientes e parceiros</h2>
+          <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+        </div>
+
+        <div class="clients-slider swiper">
+          <div class="swiper-wrapper align-items-center">
+            <div class="swiper-slide"><img src="assets/img/clients/client-1.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/cliente-10.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-2.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-6.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-9.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-4.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-3.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-5.png" class="img-fluid" alt=""></div>
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section><!-- End Clients Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+
+          <div class="col-lg-8 col-md-6 footer-contact">
+            <h4>Contatos</h4>
+             
+            <p>
+              <strong>Telefone:</strong> (21) 4108-1001
+            </p>
+
+            <p>
+              <strong>E-mail:</strong> comercialrj@grupopartnership.com.br
+            </p>
+
+            <p>
+              <strong>Unidade Itabotaí :</strong> Av. Afonso Salles, Lote 336 Qd L galpão 1 - <br>Apolo II, Itaboraí - RJ, 24858-55, Brasil
+            </p>
+
+            <p>
+              <strong>Unidade Goiânia :</strong> Avenida São Francisco, 351, QD 35, LT 05, <br>Santa Genoveva, Goiânia - GO​
+            </p>
+
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-info">
+            <h3>Redes sociais</h3>
+            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+            <div class="social-links mt-3">
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+            <div class="mt-2">
+              <a href="#" style="text-decoration:underline;">Trabalhe conosco</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Grupo Partnership</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by Bruno Quadros
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
